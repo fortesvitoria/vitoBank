40bef4181a5f7128a2a817787ce7defaffe5c6ad
Vitória Fortes <fortesvitoria@gmail.com>
2023-05-04 12:26:54 -0300

    mostra mensagens de erro na tela

--- js/script.js
+++ js/script.js
@@ -48,6 +48,10 @@ const mensagens = {
 }
 
 function verificaCampo(campo) {
+
+    let mensagem = "";
+    campo.setCustomValidity('');
+
     if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPF(campo);
     }
@@ -55,5 +59,20 @@ function verificaCampo(campo) {
         ehMaiorDeIdade(campo);
     }
 
-    console.log(campo.validity)
+    tiposDeErro.forEach(erro => {
+        if (campo.validity[erro]) {
+            mensagem = mensagens[campo.name] [erro];
+            console.log(mensagem);
+        }
+    })
+
+    const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
+    const validadorDeInput = campo.checkValidity();
+
+    if (!validadorDeInput) {
+        mensagemErro.textContent = mensagem;
+    } else {
+        mensagemErro.textContent = "";
+    }
+
 }
--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,10 +1,8 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
     if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        console.log("Esse cpf não existe")
-    } else {
-        console.log("Esse cpf existe")
-    }
+        campo.setCustomValidity('Este CPF não é válido');
+    } 
 }
 
 function validaNumerosRepetidos(cpf) {
